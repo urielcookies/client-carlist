e65f0c9154f4fad30d63d8589cb876f25f0c84d1
Ever <uriel621@live.com>
2019-03-21 22:28:21 -0700

    Add events when clicking on image

--- src/components/DetailsForm/CarImages/CarImages.js
+++ src/components/DetailsForm/CarImages/CarImages.js
@@ -1,13 +1,79 @@
 import React, {useState} from 'react';
-import {Image, Divider, Card} from 'semantic-ui-react'
+import axios from 'axios';
+import {Image, Divider, Card, Button, Modal, Header} from 'semantic-ui-react'
 
 const CarImages = (props) => {
+  const [selectedImage, setSelectedImage] = useState('');
+  console.clear()
+  const index = selectedImage.indexOf('images')
+  console.log('selectedImage', selectedImage.substring(index));
+
+  const [open, setOpen] = useState(false);
+
+  const show = () => setOpen(true)
+  const close = () => setOpen(false)
+
+  const handleImage = (image) => {
+    show();
+    setSelectedImage(image);
+  };
+
+  const deleteImage = () => {
+    close();
+    const index = selectedImage.indexOf('images')
+    const path = selectedImage.substring(index);
+    console.log('path', path);
+
+    const formData = new FormData();
+    formData.append('path', path);
+  
+    axios.post(`http://127.0.0.1:5000/deleteimage`, formData, {
+      headers: {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+      'Accept': '*',
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+
   return (
-    <Card.Group stackable itemsPerRow={3}>{
-      props.images.map((image) => (
-        <Card color='teal' key={image} raised image={image} />
-      ))
-    }</Card.Group>
+    <div>
+      <Button onClick={() => console.log('Add Image Modal')} fluid>Add More Images</Button>
+      <Divider />
+      <Card.Group stackable itemsPerRow={3}>{
+        props.images.map((image) => (
+          <Card onClick={() => handleImage(image)} color='teal' key={image} raised image={image} />
+        ))
+      }</Card.Group>
+
+      <Modal dimmer="blurring" open={open} onClose={close}>
+        <Modal.Header>Select a Photo</Modal.Header>
+        <Modal.Content image>
+          <Image wrapped size='massive' src={selectedImage} />
+          <Modal.Description>
+            <Header>Delete Image</Header>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color='black' onClick={close}>
+            Nope
+          </Button>
+          <Button
+            positive
+            icon='trash'
+            labelPosition='right'
+            content="Yes, Delete"
+            onClick={deleteImage}
+          />
+        </Modal.Actions>
+      </Modal>
+    </div>
   );
 };
 
