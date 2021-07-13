aceb5e048dbcd48fa8d4ecdac0af34080f49e08c
Ever Uriel Garcia <urielcookies@outlook.com>
2021-07-12 23:00:31 -0600

    Zindex added

--- src/components/PageLoad/PageLoad.tsx
+++ src/components/PageLoad/PageLoad.tsx
@@ -1,7 +1,7 @@
 import { Backdrop, CircularProgress } from '@material-ui/core';
 
 const PageLoad = () => (
-  <Backdrop open>
+  <Backdrop open style={{ zIndex: 1 }}>
     <CircularProgress color="inherit" />
   </Backdrop>
 );
